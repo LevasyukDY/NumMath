1c40003adde0a8befb5b2853eb9c412756909e41
LevasyukDY <Levasyuk@gmail.com>
2020-05-07 19:29:50 +0900

    .

---
+++ Поиск точки минимума/SearchMinValue/SearchMinValue.cpp
@@ -0,0 +1,119 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+const float eps = 0.001;
+
+float f(float x)
+{
+	return pow(1.3, sqrt(x*x - x + 2));
+}
+
+void SvenMethod(float &a, float &b, float x0, float d)
+{
+	float xN;
+	float xC = x0;
+	float xL = x0 - d;
+	float xR = x0 + d;
+
+	while (!((f(xL) > f(xC)) && (f(xR) > f(xC))))
+	{
+		if ((f(xL) > f(xC)) && (f(xC) > f(xR)))
+		{
+			xN = xR + 2 * d;
+			xL = xC;
+			xC = xR;
+			xR = xN;
+		} else {
+			if ((f(xL) < f(xC)) && (f(xC) < f(xR)))
+			{
+				xN = xL - 2 * d;
+				xR = xC;
+				xC = xL;
+				xL = xN;
+			}
+		}
+		d = 2 * d;
+	}
+	a = xL;
+	b = xR;
+}
+
+float PassiveSearch(const float& a, const float& b)
+{
+	unsigned k = 1;
+
+	float min = f(a),
+		n = (b - a) / (((b - a) / eps) + 1),
+		xK = a + (k * n);
+
+	while (f(xK) < min)
+	{
+		min = f(xK);
+		k++;
+		xK = a + (k * n);
+	}
+	return xK;
+}
+
+float Halving(float a, float b) 
+{
+	float x1, x2, x3, x = 0;
+
+	while (b - a > 2 * eps) 
+	{
+		x1 = (3 * a + b) / 4;
+		x2 = (a + b) / 2;
+		x3 = (a + 3 * b) / 4;
+		if ((f(x1) < f(x2)) && (f(x2) < f(x3)))
+			b = x2;
+		else
+			if ((f(x1) > f(x2)) && (f(x2) > f(x3)))
+				a = x2;
+			else
+			{
+				a = x1;
+				b = x3;
+			}
+		x = (a + b) / 2;
+	}
+	return x;
+}
+
+float Golden(float a, float b) 
+{
+	float phi = (sqrt(5) - 1) / 2,
+		x1, x2, x_min = 0;
+	while (b - a > 2 * eps) 
+	{
+		x1 = phi * a + (1 - phi) * b;
+		x2 = (1 - phi) * a + phi * b;
+		if (f(x1) < f(x2))
+			b = x2;
+		else
+			a = x1;
+		x_min = (a + b) / 2;
+	}
+	return x_min;
+}
+
+
+int main()
+{
+	float a = 0, b = 0;
+
+	SvenMethod(a, b, -1, 0.01);
+
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl << endl;
+
+	cout << PassiveSearch(a, b) << endl;
+
+	cout << Halving(a, b) << endl;
+
+	cout << Golden(a, b) << endl;
+
+	system("pause");
+	return 0;
+}
---
+++ Численное интегрирование/Numerical_Integration/Numerical_Integration.cpp
@@ -0,0 +1,114 @@
+﻿#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+// Первая функция
+float f1(float x)
+{
+	return 1 / sqrt(pow(x, 2) + 0.5);
+}
+
+// Вторая функция
+float f2(float x)
+{
+	return cos(x) / (x + 2);
+}
+
+// Вторая производная первой функции
+float f1_xx(float x)
+{
+	return (2 * (pow(x, 2) - 0.25)) / pow(0.5 + pow(x, 2), 5 / 2);
+}
+
+
+float max_f1_xx(float a, float b, float eps)
+{
+	float k = ceil((b - a) / eps),
+		d = (b - a) / k;
+	vector<float> vec;
+
+	for (int i = 0; i < k + 1; i++)
+		vec.push_back(f1_xx(a + i * d));
+
+	return *max_element(vec.begin(), vec.end());
+}
+
+
+float trapezoid_method(float a, float b, float eps, float max)
+{
+	int n = sqrt(max * pow(b - a, 3) / (12 * eps));
+	float h = (b - a) / n,
+		w = 0, x;
+
+	for (int k = 1; k <= n - 1; k++)
+	{
+		x = a + k * h;
+		w += f1(x);
+	}
+	w = (2 * w + f1(a) + f1(b)) * h / 2;
+
+	return w;
+}
+
+
+float simpson_method(float a, float b, float m)
+{
+	float n = 2 * m,
+		h = (b - a) / n,
+		s = 0,
+		s1 = 0,
+		s2 = 0, x;
+
+	for (int k = 1; k <= m; k++)
+	{
+		x = a + (2 * k - 1) * h;
+		s1 += f2(x);
+	}
+
+	for (int k = 1; k <= m - 1; k++)
+	{
+		x = a + 2 * k * h;
+		s2 += f2(x);
+	}
+
+	s = (4 * s1 + 2 * s2 + f2(a) + f2(b)) * h / 3;
+
+	return s;
+}
+
+
+int main()
+{
+	setlocale(0,"");
+
+	float a = 1.2f;
+	float b = 2.4f;
+	const float eps = 0.001f;
+	float max = 9;
+
+	cout << "Первый интеграл = " << trapezoid_method(a, b, eps, max) << endl;
+
+	float a1 = 0.4f;
+	float b1 = 1.2f;
+	float m = 8;
+
+	float I1 = simpson_method(a1, b1, m);
+	m++;
+	float I2 = simpson_method(a1, b1, m);
+
+	// Оценка погрешности
+	while (fabs(I1 - I2) > eps)
+	{
+		I1 = I2;
+		m++;
+		I2 = simpson_method(a1, b1, m);
+	}
+
+	cout << "Второй интеграл = " << I2 << endl << endl;
+
+	system("pause");
+	return 0;
+}
